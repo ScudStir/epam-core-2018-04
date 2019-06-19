6d03eca6dd891f686d35f0da4b27144edc406e65
ScudStir <54647541jinG>
2019-06-19 22:33:29 +0300

    Completed 7, 8 task

--- src/main/java/com/scudstir/tasks/Task7.java
+++ src/main/java/com/scudstir/tasks/Task7.java
@@ -1,33 +1,84 @@
 package com.scudstir.tasks;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
 public class Task7 {
+    private static boolean isResult(String string) {
+        if (string.matches("[a-zA-Z]*")) {
+            char[] chars = string.toLowerCase().toCharArray();
+            HashSet<Character> set = new HashSet<>();
+            for (char aChar : chars) {
+                if (!set.add(aChar)) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private static boolean addIgnoreCase(String str, LinkedHashSet<String> set) {
+        for (String s : set) {
+            if (str.equalsIgnoreCase(s)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     /**
      * Ввести N слов с консоли.
      * Найти слова, состоящие только из различных символов английского алфавита.
      * Символы верхнего и нижнего регистра считать одинаковыми.
      * В случае, если слово встречается более одного раза - вывести его единожды.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество слов в строке
      * Строка, содержащая нимимум N слов (состоящих только из букв английского языка), разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должна быть выведена строка, содержащая найденные слова, разделенные пробелами.
      * Порядок слов должен совпадать с порядком их появления во входной последовательности.
      * В случае, если не найдено ни одного слова, удовлетворяющего условию - в поток должно быть выведено "NOT FOUND".
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 11
      * The Java programming language is a general-purpose, concurrent, class-based, object-oriented language
-     *
+     * <p>
      * Выходные данные:
      * The is a
      */
     public static void main(String[] args) {
-        // TODO реализация
+        System.out.println("Введите количество слов:");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println("Введите строку:");
+        Scanner scanner1 = new Scanner(System.in);
+        LinkedHashSet<String> linkedHashSet = new LinkedHashSet<>();
+        for (int i = 0; i < num; i++) {
+            String string = scanner1.next();
+            if (isResult(string) && addIgnoreCase(
+                    string, linkedHashSet
+            )) {
+                linkedHashSet.add(string);
+            }
+        }
+
+
+        if (linkedHashSet.isEmpty()) {
+            System.out.println("NOT FOUND");
+        } else {
+            for (String str : linkedHashSet) {
+                System.out.print(str + " ");
+            }
+        }
     }
 }
--- src/main/java/com/scudstir/tasks/Task8.java
+++ src/main/java/com/scudstir/tasks/Task8.java
@@ -1,6 +1,23 @@
 package com.scudstir.tasks;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Task8 {
+    private static boolean isNumber(String str) {
+        return str.matches("[0-9]*");
+    }
+
+    private static boolean isPalindrome(String str) {
+        int length = str.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (str.charAt(i) != str.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
 
     /**
      * Ввести N слов с консоли.
@@ -8,26 +25,51 @@ public class Task8 {
      * Среди них необходимо найти число-палиндром (одинаково читающееся в обоих направлениях).
      * Число, содержащее одну цифру также является палиндромом.
      * Если палиндромов во входной последовательности больше одного - найти второй уникальный из них.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество слов в строке
      * Строка, содержащая минимум N слов, разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должно быть выведено найденное число-палиндром.
      * В случае, если не найдено ни одного палиндрома - в поток должно быть выведено "NOT FOUND".
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 15
      * Chapter 11 describes exceptions, chapter 13 describes binary compatibility... chapter 22 presents a syntactic grammar
-     *
+     * <p>
      * Выходные данные:
      * 22
      */
     public static void main(String[] args) {
-        // TODO реализация
+        System.out.println("Введите количество слов:");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println("Введите строку:");
+        Scanner scanner1 = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            String string = scanner1.next();
+            if (isNumber(string) && isPalindrome(string)) {
+                if (list.size() == 2) {
+                    break;
+                } else if (list.contains(string)) {
+                    continue;
+                }
+                list.add(string);
+            }
+        }
+        if (list.isEmpty()) {
+            System.out.println("NOT FOUND");
+        } else if (list.size() == 1) {
+            System.out.println(list.get(0));
+        } else {
+            System.out.println(list.get(1));
+        }
+
+
     }
 }
